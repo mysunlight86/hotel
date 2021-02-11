7f78aabf1b3c047c2267600e233d6c8b2c3bfa75
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-02-11 19:59:47 +0300

    feat: specify a template for generating html file

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            title: 'test'
+            title: 'test',
+            template: './src/index.html'
         })
     ],
 
