2ce30354b139658c66aad287c5e20a79c7a8c4e7
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-02-11 16:27:16 +0300

    feat: install webpack locally in devDependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const webpack = require('webpack');
+// const NODE_ENV = process.env.NODE_ENV || 'development';
+// const webpack = require('webpack');
 
 module.exports = {
-    mode: NODE_ENV,
+    // mode: NODE_ENV,
 
-    entry: './home',
-    output: {
-        filename: 'build.js',
-        library: 'home'
-    },
+    // entry: './home',
+    // output: {
+    //     filename: 'build.js',
+    //     library: 'home'
+    // },
 
-    watch: NODE_ENV === 'development',
+    // watch: NODE_ENV === 'development',
 
-    watchOptions: {
-        aggregateTimeout: 100
-    },
+    // watchOptions: {
+    //     aggregateTimeout: 100
+    // },
 
-    devtool: NODE_ENV === 'development' ? 'inline-cheap-module-source-map' : undefined,
+    // devtool: NODE_ENV === 'development' ? 'inline-cheap-module-source-map' : undefined,
 
-    plugins: [
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        })
-    ],
+    // plugins: [
+    //     new webpack.DefinePlugin({
+    //         NODE_ENV: JSON.stringify(NODE_ENV)
+    //     })
+    // ],
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-        ],
-    }
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js$/,
+    //             exclude: /node_modules/,
+    //             use: ['babel-loader'],
+    //         },
+    //     ],
+    // }
 };
