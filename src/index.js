a00ebd9f69be1521f49f97fd755ccb357d2bc327
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-04-02 19:01:32 +0300

    feat: add buttons, lists and switches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './components/text-field/maskedinput.js';
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import './components/range-slider/range-slider.js';
 
 import './vendor/index.scss';
