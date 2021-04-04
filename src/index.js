da61f2682f4ae61119dbc42262328ac8db513611
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-04-04 17:36:23 +0300

    feat: add datepicker and comfort block

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import 'webpack-jquery-ui';
 import 'webpack-jquery-ui/css';
 import './components/range-slider/range-slider.js';
 import './components/dropdown/dropdown.js';
+import './components/datepicker/datepicker.js';
 
 import './vendor/index.scss';
